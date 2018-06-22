86865ebe00e6f7164cc33e3b6a508db10238f999
yosuke.yuasa <yosuke.yuasa@rakuten.com>
2018-06-22 01:14:38 +0000

    ranking

--- database/migrations/2018_06_21_095751_create_item_user_table.php
+++ database/migrations/2018_06_21_095751_create_item_user_table.php
@@ -6,11 +6,7 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateItemUserTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
+    
     public function up()
     {
         Schema::create('item_user', function (Blueprint $table) {
@@ -25,11 +21,6 @@ class CreateItemUserTable extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::dropIfExists('item_user');
